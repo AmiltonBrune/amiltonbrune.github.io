c07a953de47cb3115314d6435f34d8a6eb711f17
Amilton Brune da Silva Junior <amilton.junior@digisystem.com.br>
2019-08-09 19:56:00 -0300

    Testando página github com react

---
+++ precache-manifest.3e1698a85e79166a417b732e3b56f11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb52a332f9b89c2eec8d2b8737f0110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a70388d1c1b0127e576",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "646716197de9d5ef2cbe",
+    "url": "/static/js/2.45f8e149.chunk.js"
+  },
+  {
+    "revision": "9a70388d1c1b0127e576",
+    "url": "/static/js/main.48193586.chunk.js"
+  },
+  {
+    "revision": "aabb07e5c94e3005f559",
+    "url": "/static/js/runtime~main.e0448750.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
